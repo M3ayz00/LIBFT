f476af1b18d6e881f16fec36f106ceb09dc9f785
M3ayz00 <78887998+M3ayz00@users.noreply.github.com>
2023-11-22 20:09:30 +0100

    Add files via upload
    push_time

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaadidi <msaadidi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 18:06:16 by msaadidi          #+#    #+#             */
+/*   Updated: 2023/11/21 20:01:42 by msaadidi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t num_elements, size_t element_size)
+{
+	void	*a;
+
+	if (SIZE_MAX / num_elements < element_size && num_elements != 0)
+		return (NULL);
+	a = malloc(num_elements * element_size);
+	if (!a)
+		return (NULL);
+	if (num_elements == 0 || element_size == 0)
+		return (a);
+	ft_memset(a, 0, element_size * num_elements);
+	return (a);
+}
